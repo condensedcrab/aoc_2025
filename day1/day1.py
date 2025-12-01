10bee1892b1810cc4654019e9e65f5361ac47d33
David <david.barbalas@gmail.com>
2025-12-01 10:16:45 -0600

    fleshing out the code

--- day1/day1.py
+++ day1/day1.py
@@ -1,4 +1,26 @@
-# %% 
+# %% set up class 
 
 import numpy 
 
+class Dial:
+    
+    def __init__(self):
+        self.dial_pos = 50 # range is 0-99
+        self.dial_zero_count = 0
+        
+        return
+    
+    def rotate_circle(self, input):
+        sign_pos = True
+        
+        # parse the polarity of the input
+        if "R" in input:
+            sign_pos = True     # R is CW, towards higher
+        elif "L" in input:
+            sign_pos = False    # L is CCW, towards lower
+    
+        # now determine what the new position is and update 0 crossing if needed
+        
+    
+        return
+# %%

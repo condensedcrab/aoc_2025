ebfbf998445b60287193d4027b05ba123ff4bb54
David <david.barbalas@gmail.com>
2025-12-02 15:52:11 -0600

    example input parsing done

--- day2/day2.py
+++ day2/day2.py
@@ -1,8 +1,31 @@
-# import common packages
+# %% import common packages
 
 import numpy as np
 import re
 
 
+class Day2():
+
+    def __init__(self):
+        
+        return
+    
 
 
+
+# %% part 1 input read-in
+import csv
+input = []
+with open('input_day2.txt', mode='r', encoding='utf-8') as file:
+    # Create a CSV reader object
+    csv_reader = csv.reader(file)
+
+    # Iterate through each row in the CSV file
+    for row in csv_reader:
+        for r in row:
+            if r == "":
+                continue
+            else:
+                input.append(r)
+
+print(input)
